dc471acee1e199a5299b2e3da58062b6c4592689
Joseph Michaels <jmichaels@gmail.com>
2010-02-27 19:57:16 -1000

    I can't read

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void collantz(int n) {
+void collatz(int n) {
 	printf(" %d",n);
 	if (n <= 1) return;
 	if (n % 2 == 0) {
-		collantz(n/2);
+		collatz(n/2);
 	} else {
-		collantz((3 * n) + 1);
+		collatz((3 * n) + 1);
 	}
 }
 
 int main() {
 	int i;
 	for (i = 100000; i < 1000000; i++) {
-		collantz(i);
+		collatz(i);
 		printf("\n");
 	}
 	return 0;
